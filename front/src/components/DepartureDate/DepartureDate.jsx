a67610066dd5437cee2bdc3d38e8598e5c8385a8
ofkande <imddande@gmail.com>
2020-03-01 13:31:28 +0900

    set calendar

---
+++ front/src/components/DepartureDate/DepartureDate.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Calendar from 'react-calendar';
+import './DepartureDate.css';
+
+class DepartureDate extends React.Component {
+    state = {
+        date: new Date(),
+    }
+
+    onChange = date => this.setState({ date })
+
+    render() {
+        return (
+            <div className="departureDate">
+                <button className="departureDate__button" onClick={ToggleCalender}>출발일</button>
+                <Calendar
+                    onChange={this.onChange}
+                    value={this.state.date}
+                />
+            </div>
+        );
+    }
+}
+
+function ToggleCalender() {
+    const calendar = document.querySelector('.react-calendar');
+    if (calendar.style.display === 'none') {
+        calendar.style.display = 'block';
+    } else if (calendar.style.display === '') {
+        calendar.style.display = 'block';
+    } else {
+        calendar.style.display = 'none';
+    }
+}
+
+export default DepartureDate;
