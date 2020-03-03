0b2a7ca3221bbb566fb9212babd2f3b4ce63340e
forbid403 <forbid403@naver.com>
2020-03-03 17:22:59 +0900

    [add] add google geochart

--- front/src/pages/MapPage.jsx
+++ front/src/pages/MapPage.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import { Chart } from "react-google-charts"
+const googleAPIKey = "AIzaSyC5JTn-jFdZ3t68S049uTSnTOCdXmvHg_A"
 
 const Map = () => {
-  return <div>지도로 보기</div>
+  return <div>
+    <Chart
+      width={'700px'}
+      height={'700px'}
+      chartType="GeoChart"
+      mapsApiKey={googleAPIKey}
+      data={[
+        ['Country', 'Popularity'],
+        ['Germany', 200],
+        ['United States', 300],
+        ['Brazil', 400],
+        ['Canada', 500],
+        ['France', 600],
+        ['RU', 700],
+      ]}
+    />
+  </div>
 }
 
 export default Map
