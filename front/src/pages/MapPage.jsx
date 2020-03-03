e130376b4ba8e2560ee52d305362fe3ac7a48c08
forbid403 <forbid403@naver.com>
2020-03-03 17:27:57 +0900

    [change] to class

--- front/src/pages/MapPage.jsx
+++ front/src/pages/MapPage.jsx
@@ -1,25 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Chart } from "react-google-charts"
 const googleAPIKey = "AIzaSyC5JTn-jFdZ3t68S049uTSnTOCdXmvHg_A"
 
-const Map = () => {
-  return <div>
-    <Chart
-      width={'700px'}
-      height={'700px'}
-      chartType="GeoChart"
-      mapsApiKey={googleAPIKey}
-      data={[
-        ['Country', 'Popularity'],
-        ['Germany', 200],
-        ['United States', 300],
-        ['Brazil', 400],
-        ['Canada', 500],
-        ['France', 600],
-        ['RU', 700],
-      ]}
-    />
-  </div>
+class MapPage extends Component {
+  render() {
+    return <div>
+      <Chart
+        width={'700px'}
+        height={'700px'}
+        chartType="GeoChart"
+        mapsApiKey={googleAPIKey}
+        data={[
+          ['Country', 'Popularity'],
+          ['Germany', 200],
+          ['United States', 300],
+          ['Brazil', 400],
+          ['Canada', 500],
+          ['France', 600],
+          ['RU', 700],
+        ]}
+      />
+    </div>
+  }
 }
 
-export default Map
+export default MapPage
