e104daac3aa5723593eb49484bfa242ae47010f7
forbid403 <forbid403@naver.com>
2020-03-03 17:38:17 +0900

    [add] bootstrap for dynamic web

--- front/src/pages/MapPage.jsx
+++ front/src/pages/MapPage.jsx
@@ -1,25 +1,28 @@
 import React, { Component } from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 import { Chart } from "react-google-charts"
 const googleAPIKey = "AIzaSyC5JTn-jFdZ3t68S049uTSnTOCdXmvHg_A"
 
 class MapPage extends Component {
   render() {
     return <div>
-      <Chart
-        width={'700px'}
-        height={'700px'}
-        chartType="GeoChart"
-        mapsApiKey={googleAPIKey}
-        data={[
-          ['Country', 'Popularity'],
-          ['Germany', 200],
-          ['United States', 300],
-          ['Brazil', 400],
-          ['Canada', 500],
-          ['France', 600],
-          ['RU', 700],
-        ]}
-      />
+      <Container>
+        <Chart
+          width={'100%'}
+          height={'100%'}
+          chartType="GeoChart"
+          mapsApiKey={googleAPIKey}
+          data={[
+            ['Country', 'Popularity'],
+            ['Germany', 200],
+            ['United States', 300],
+            ['Brazil', 400],
+            ['Canada', 500],
+            ['France', 600],
+            ['RU', 700],
+          ]}
+        />
+      </Container>
     </div>
   }
 }
