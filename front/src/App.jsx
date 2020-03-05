f64d38bec0e87e5415450f4598052c5e0a5f8dd4
joabyjoa <joabyjoa@gmail.com>
2020-03-05 23:18:33 +0900

    CS-031 조회 결과 페이지

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import { ROUTES } from './constants';
 import { Navigation } from './components';
+import BgContext from './contexts/BgContext';
 
 const App = () => {
+  const { color } = useContext(BgContext);
   return (
     <>
       <Navigation />
-      <main>
+      <main style={{ background: color }}>
         {ROUTES.map(({ path, component }) => (
           <Route
             key={path}
