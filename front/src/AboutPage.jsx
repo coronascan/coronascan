d4aef3e734441fe34c8bc55ef909cf434207053c
ofkande <imddande@gmail.com>
2020-03-11 18:49:54 +0900

    about us

--- front/src/AboutPage.jsx
+++ front/src/AboutPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import AboutUs from '../components/AboutUs/AboutUs';
+import AboutUs from './components/AboutUs/AboutUs';
 
 function renderAboutUs({ name, img, position, info, link }) {
   return <AboutUs key={name} name={name} img={img} position={position} info={info} link={link} />
@@ -8,42 +8,42 @@ function renderAboutUs({ name, img, position, info, link }) {
 const MemberList = [
   {
     name: 'Devv',
-    img: 'https://i.picsum.photos/id/867/200/300.jpg',
+    img: 'devv',
     position: "Front-end Developer, Design",
-    info: "지속적인 발전을 추구하는 개발자입니다. 문제 해결을 좋아하고 취미로 디자인을 합니다.",
+    info: "문제 해결을 좋아하고 취미로 디자인을 하는 개발자입니다. 새로운 도전과 지속적인 발전을 추구합니다.",
+    link: ""
+  },
+  {
+    name: 'greyay',
+    img: 'greyay',
+    position: "Developer",
+    info: "배우고 성장하며 얻은 지식과 기술을 나눌 수 있는 개발자가 되고 싶습니다.",
     link: ""
   },
   {
     name: 'joabyjoa',
-    img: 'https://i.picsum.photos/id/866/200/300.jpg',
+    img: 'joabyjoa',
     position: "Front-end Developer",
-    info: "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur",
+    info: "",
     link: ""
   },
   {
     name: 'Nahia',
-    img: 'https://i.picsum.photos/id/824/200/300.jpg',
+    img: 'nahia',
     position: "PM, Back-end Developer",
     info: "열심히 사는 삶을 지향하는 개발자",
     link: ""
   },
   {
     name: 'Sonia / 배성희',
-    img: 'https://i.picsum.photos/id/85/200/300.jpg',
+    img: 'sonia',
     position: "Developer",
     info: "사람들에게 도움이 되는 개발자가 되고 싶습니다. 코로나 바이러스가 하루빨리 해결 되기를 바랍니다!",
     link: "www.github.com/forbid403"
   },
   {
-    name: '호퍼',
-    img: 'https://i.picsum.photos/id/81/200/300.jpg',
-    position: "position",
-    info: "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur",
-    link: ""
-  },
-  {
     name: 'Aim B',
-    img: 'https://i.picsum.photos/id/1/200/300.jpg',
+    img: 'aimb',
     position: "Product Manager",
     info: "세상을 더 나은 곳으로 바꿔가고 싶습니다.",
     link: "Instagram @andyoureturntome"
--- front/src/components/AboutUs/AboutUs.jsx
+++ front/src/components/AboutUs/AboutUs.jsx
@@ -4,9 +4,8 @@ import './AboutUs.css';
 function AboutUs({ name, img, position, info, link }) {
     return <div className="member-box">
         <div className="member-box__member">
-            <img
-                src={img}
-                alt={name}
+            <div
+                className={"member__img-" + img}
             />
             <div className="member__name-box">
                 <h3>{name}</h3>
