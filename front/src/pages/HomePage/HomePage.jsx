a3a7e79f990528b59f1618f644e49d57bcdf84bb
ofkande <imddande@gmail.com>
2020-03-12 20:07:29 +0900

    css
    입국 금지국 횟수 css
    section center 정렬 수정

--- front/src/pages/HomePage/HomePage.jsx
+++ front/src/pages/HomePage/HomePage.jsx
@@ -31,6 +31,16 @@ const HomePage = props => {
   return (
     <section className="homepage">
       <h2>✈️ 입국 가능 국가 조회하기</h2>
+      <div className="count__parent">
+        <div className="prohibitions">
+          <h3>입국 금지 국가</h3>
+          <h3><CountUp end={prohibitions} /></h3>
+        </div>
+        <div className="restrictions">
+          <h3>입국 제한 국가</h3>
+          <h3><CountUp end={restrictions} /></h3>
+        </div>
+      </div>
       <div className="homepage__contents">
         <h3>입국을 원하는 지역, 국가를 입력해주세요</h3>
         <AutoComplete onChange={setCountry} />
@@ -45,16 +55,6 @@ const HomePage = props => {
             const country = input.value;
             getResult('map', country)
           }}>지도로 확인하기</Button>
-        </div>
-      </div>
-      <div className="count__parent">
-        <div className="prohibitions">
-          <h2>입국 금지 국가</h2><br/>
-          <h3><CountUp end={prohibitions} /></h3>
-        </div>
-        <div className="restrictions">
-          <h2>입국 제한 국가</h2><br/>
-          <h3><CountUp end={restrictions} /></h3>
         </div>
       </div>
     </section>
