f29a2687c694cca141a97e2c30674efaa09f3597
ofkande <imddande@gmail.com>
2020-03-13 16:06:03 +0900

    warning page css
    코로나 관련 외교부 여행 권고사항 페이지 box style 변경

--- front/src/pages/WarningPage/WarningPage.jsx
+++ front/src/pages/WarningPage/WarningPage.jsx
@@ -40,8 +40,8 @@ class WarningPage extends Component {
         <div className="warning__container">
           <Container>
             <Row>
-              <h3>여행 단계별 행동 요령</h3>
               <ul className="warning__step">
+                <li className="warning__title"><h3>여행 단계별 행동 요령</h3></li>
                 <li>1단계(남색경보) : (체류자) 신변안전 유의, (여행예정자) 여행 유의</li>
                 <li>2단계(황색경보) : (체류자) 신변안전 특별유의, (여행예정자) 여행 필요성 신중 검토</li>
                 <li>3단계(적색경보) : (체류자) 긴급용무가 아닌 한 철수, (여행예정자) 여행 취소․연기</li>
@@ -49,8 +49,8 @@ class WarningPage extends Component {
               </ul>
             </Row>
             <Row>
-              <h3>최신 여행 경보 단계</h3>
               <ul className="warning__step">
+                <li className="warning__title"><h3>최신 여행 경보 단계</h3></li>
                 <li>일본 일본 전 지역(후쿠시마 원전 주변지역 제외)에 남색경보 발령 (02.29)</li>
                 <li>이탈리아 	이탈리아 북부지역 3개 주(롬바르디아주, 베네토주, 에밀리아-로마냐주)에 황색경보 발령 (02.28)</li>
                 <li>중국, 홍콩(중국), 마카오(중국) 전 중국지역에 여행경보 2단계(여행자제) 발령 (01.28)</li>
