e0f5ac65cd70f6f08673dba47c8bf7831be6b3c0
grey.ling.dev <grey.ling.dev@gmail.com>
2020-03-15 18:42:35 +0900

    지도상 경보별 색상 수정

--- front/src/components/MapPage/Map.jsx
+++ front/src/components/MapPage/Map.jsx
@@ -3,24 +3,24 @@ import { Chart } from "react-google-charts"
 const googleAPIKey = "AIzaSyBTvsuJcbhSf2giulYdP66791797JE4ZTA"
 
 const options = {
-    colorAxis: { colors: ['black', 'red'] },
+    colorAxis: { colors: ['3D6298', 'BDAF7E', 'A43F3D'] },
     tooltip: { isHtml: true, trigger: "visible" }
 };
 
-class Map extends Component{
+class Map extends Component {
 
     render() {
-        return(
-        <div>
-        <Chart
-          width={'100%'}
-          height={'100%'}
-          chartType="GeoChart"
-          mapsApiKey={googleAPIKey}
-          data={this.props.countries}
-          options={options}
-        />
-        </div>)
+        return (
+            <div>
+                <Chart
+                    width={'100%'}
+                    height={'100%'}
+                    chartType="GeoChart"
+                    mapsApiKey={googleAPIKey}
+                    data={this.props.countries}
+                    options={options}
+                />
+            </div>)
     }
 
 
