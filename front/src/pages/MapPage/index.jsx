3fe18ace4437f3f669cc254e0f5304507500cc26
ofkande <ofkande@gmail.com>
2020-04-14 16:26:02 +0900

    색인 추가
    *한국인 입국 제한 조치 시행국 중 90개 국가에 대해 입국 제한 조치 시행

--- front/src/pages/MapPage/index.jsx
+++ front/src/pages/MapPage/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useContext } from 'react';
-import ReactTooltip from "react-tooltip";
-import MapChart from '../../components/MapPage/MapChart'
-import Config from '../../config/config'
+import ReactTooltip from 'react-tooltip';
+import MapChart from '../../components/MapPage/MapChart';
+import Config from '../../config/config';
 import ResultContext from '../../contexts/ResultContext';
 import './MapPage.css';
 
@@ -9,8 +9,7 @@ let items = [];
 let data = [];
 
 export function MapPage(props) {
-
-  const [content, setContent] = useState("");
+  const [content, setContent] = useState('');
   const [countries, setData] = useState([]);
   const [hidden, setHide] = useState(false);
 
@@ -34,7 +33,7 @@ export function MapPage(props) {
   }
 
   useEffect(() => {
-    changeBg("#FBFBFC");
+    changeBg('#FBFBFC');
     fetchUrl();
   }, []);
 
@@ -46,32 +45,35 @@ export function MapPage(props) {
 
       <ul className="legend__container">
         <li className="legend">
-          <span className="legend_box bg-red04" />입국 금지
+          <span className="legend_box bg-red04" />
+          입국 금지
         </li>
         <li className="legend">
-          <span className="legend_box bg-red02" />입국 제한
+          <span className="legend_box bg-red02" />
+          입국 제한
         </li>
         <li className="legend">
-          <span className="legend_box bg-grey" />입국 가능
+          <span className="legend_box bg-grey" />
+          입국 가능
         </li>
       </ul>
-
+      <div style={{ textAlign: 'center', fontSize: 14 }}>
+        *한국인 입국 제한 조치 시행국 중 90개 국가에 대해 입국 제한 조치 시행
+      </div>
       <div className="map-area">
         <MapChart
           selected={target}
           countries={countries}
           setTooltipContent={setContent}
-          setTooltipHide={(ret) => {
-            setHide(ret)
-            ReactTooltip.hide(hidden)
+          setTooltipHide={ret => {
+            setHide(ret);
+            ReactTooltip.hide(hidden);
           }}
         />
         <ReactTooltip>{content}</ReactTooltip>
       </div>
-
     </section>
   );
 }
-
 
 export default MapPage;
